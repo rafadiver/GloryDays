6a982f7c1ef7775a0bf67bdfa0222b46e582fe8f
Maria Susana Moline Venanzi <susana@Marias-MacBook-Air.local>
2016-11-19 10:00:40 -0600

    Esta es el comité de Glory Days como prueba

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GloryDays
+//
+//  Created by Maria Susana Moline Venanzi on 11/15/16.
+//  Copyright © 2016 Maria Susana Moline Venanzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
